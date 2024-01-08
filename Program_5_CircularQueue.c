0dd5365d2fb03bc024e505792f7412e66092123e
George Emmanuel Thomas <72024420+GeorgeET15@users.noreply.github.com>
2024-01-08 09:31:07 +0530

    Update Program_5_CircularQueue.c

--- Program_5_CircularQueue.c
+++ Program_5_CircularQueue.c
@@ -1,130 +1,106 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void enqueue(int queue[10],int* front , int *rear , int size);
+int Q[100];
+int front = -1, rear = -1;
+int n, item;
 
-void dequeue(int queue[10],int* front , int *rear , int size);
-
-void display(int queue[10],int* front , int *rear , int size);
-
-
-
-
-
-
-void display(int queue[10],int* front , int *rear , int size)
-{
-	if ((*front)==(*rear))
-	{
-		printf("\e[1;1H\e[2J");
-		printf("EMPTY QUEUE");
-		printf("\nEnter a number to go back : ");
-		scanf("%d",&size);
-	}
-	else
-	{
-		int i;
-		printf("\e[1;1H\e[2J");
-		if((*front)==0)
-		{
-			(*front)++;
-			i = (*front);
-		}
-		else
-		{
-			i = (*front);
-		}
-		
-		for(i;i!=((*rear)+1);i++)
-		{
-			printf("|%d|",queue[i]);
-		}
-		printf("\nEnter a number to go back : ");
-		scanf("%d",&size);		
-	}
+int isEmpty() {
+    return (front == -1);
 }
 
-
-
-
-
-void dequeue(int queue[10],int* front , int *rear , int size)
-{
-	printf("\e[1;1H\e[2J");
-	if ((*front) == (*rear))
-	{
-		printf("EMPTY QUEUE");
-		printf("\nEnter a number to go back");
-		scanf("%d",&size);
-	}
-	else
-	{
-		(*front) = (((*front) +1)%size);
-		printf("\nEnter a number to go back");
-		scanf("%d",&size);
-	}	
+int isFull() {
+    return ((rear + 1) % n == front);
 }
 
-
-
-void enqueue(int queue[10],int* front , int *rear , int size)
-{
-	printf("\e[1;1H\e[2J");
-	
-	if ((((*rear)+1)%size) == (*front))
-	{
-		printf("THE QUEUE IS FULL");
-		scanf("%d",&size);
-	}
-	else
-	{
-		printf("Enter the element to enqueued : ");
-		(*rear)++;
-		scanf("%d",&queue[(*rear)]);
-		printf("%d inserted into index %d",queue[(*rear)] , (*rear));
-		printf("\nEnter a number to go back");
-		scanf("%d",&size);
-	}
+void enqueue(int item) {
+    if (isFull()) {
+        printf("Queue is full\n");
+    } else {
+        rear = (rear + 1) % n;
+        Q[rear] = item;
+        if (front == -1) {
+            front = rear;
+        }
+        printf("Enqueued %d onto the queue.\n", item);
+    }
 }
 
+void dequeue() {
+    if (isEmpty()) {
+        printf("Queue is empty\n");
+    } else {
+        item = Q[front];
+        printf("Dequeued %d from the queue.\n", item);
+        if (front == rear) {
+            front = rear = -1;
+        } else {
+            front = (front + 1) % n;
+        }
+    }
+}
 
+void display() {
+    if (isEmpty()) {
+        printf("Queue is empty\n");
+    } else {
+        int i = front;
+        printf("Queue elements\n");
+        do {
+            printf("%d\n", Q[i]);
+            i = (i + 1) % n;
+        } while (i != (rear + 1) % n);
+    }
+}
 
+int main() {
+    int choice;
 
+    printf("Enter the number of max elements: ");
+    scanf("%d", &n);
 
+    printf("\nMENU:\n1. Enqueue\n2. Dequeue\n3. Is Empty\n4. Is Full\n5. Display\n6. Exit\n");
 
-
-
-
-void main()
-{
-	int queue[10];
-	int front , rear, size;
-	front = rear = 0;
-	printf("Welcome to circular queue");
-	printf("\nEnter the size of the queue : ");
-	scanf("%d",&size);
-	printf("\e[1;1H\e[2J");
-	int ch = 1;
-	int trash;
-	while (ch != 0)
-	{
-		printf("\e[1;1H\e[2J");
-		printf("CIRCULAR QUEUE\n");
-		printf("   1.Enqueue\n   2.Dequeue\n   3.Display\n\n   0.Exit");
-		printf("\nEnter your option : ");
-		scanf("%d",&ch);
-		if (ch ==1)
-		{
-			enqueue(queue,&front,&rear,size);
-		}
-		if (ch ==2)
-		{
-			dequeue(queue,&front,&rear,size);
-		}
-		if (ch ==3)
-		{
-			display(queue,&front,&rear,size);
-		}
-		
-	
-	}
+    while (1) {
+        printf("Enter the choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1: {
+                printf("Enter the element to enqueue: ");
+                scanf("%d", &item);
+                enqueue(item);
+                break;
+            }
+            case 2: {
+                dequeue();
+                break;
+            }
+            case 3: {
+                if (isEmpty()) {
+                    printf("Queue is empty\n");
+                } else {
+                    printf("Queue is not empty\n");
+                }
+                break;
+            }
+            case 4: {
+                if (isFull()) {
+                    printf("Queue is full\n");
+                } else {
+                    printf("Queue is not full\n");
+                }
+                break;
+            }
+            case 5: {
+                display();
+                break;
+            }
+            case 6: {
+                printf("Terminating.\n");
+                return 0;
+            }
+            default: {
+                printf("Invalid choice!\n");
+            }
+        }
+    }
 }
